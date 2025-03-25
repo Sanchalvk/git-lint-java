75240a5b056ce2a4f4092a94a9fbb9c070d5f1b4
Sanchalvk <150319103+Sanchalvk@users.noreply.github.com>
2025-03-24 22:09:55 -1100

    Add files via upload

---
+++ demospringboot/src/main/java/springbootTogithub/demospringboot/DemospringbootApplication.java
@@ -0,0 +1,19 @@
+package springbootTogithub.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringbootApplication {
+
+	public static void main(String[] args) {
+
+		public String greet(){
+			System.out.println("Welcome to sprinboot..");
+		}
+	}
+		SpringApplication.run(DemospringbootApplication.class, args);
+
+		
+
+}
